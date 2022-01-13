fa8a8b3fddb2d14e7d99315862e39a9a891e1b53
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-13 13:23:52 +0100

    :recycle: refact test

--- test/my_resume_app_test.dart
+++ test/my_resume_app_test.dart
@@ -2,12 +2,14 @@ import 'package:antoniomdm/presentation/my_resume_app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('show name', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyResumeApp());
-    expect(find.text('Antonio Manuel Díaz Moreno'), findsOneWidget);
-  });
-  testWidgets('show under construction', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyResumeApp());
-    expect(find.text('Under Construction'), findsOneWidget);
+  group('my resume should', () {
+    testWidgets('show name', (WidgetTester tester) async {
+      await tester.pumpWidget(const MyResumeApp());
+      expect(find.text('Antonio Manuel Díaz Moreno'), findsOneWidget);
+    });
+    testWidgets('show under construction', (WidgetTester tester) async {
+      await tester.pumpWidget(const MyResumeApp());
+      expect(find.text('Under Construction'), findsOneWidget);
+    });
   });
 }
