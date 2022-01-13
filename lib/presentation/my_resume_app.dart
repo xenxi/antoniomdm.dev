bdec8035bda21d2de3097b53b01aaf558b02917c
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-13 13:16:16 +0100

    :white_check_mark: add green test: show name

---
+++ lib/presentation/my_resume_app.dart
@@ -0,0 +1,14 @@
+import 'package:antoniomdm/presentation/under_construction_page.dart';
+import 'package:flutter/material.dart';
+
+class MyResumeApp extends StatelessWidget {
+  const MyResumeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Material App',
+      home: UnderConstructionPage(),
+    );
+  }
+}
---
+++ lib/presentation/under_construction_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UnderConstructionPage extends StatelessWidget {
+  const UnderConstructionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Antonio Manuel Díaz Moreno'),
+    );
+  }
+}
