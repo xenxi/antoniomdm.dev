4662a60095e4961639f3baa2974daedf1d9934b1
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-13 13:49:26 +0100

    :lipstick: update view

--- lib/presentation/my_resume_app.dart
+++ lib/presentation/my_resume_app.dart
@@ -1,14 +1,17 @@
 import 'package:antoniomdm/presentation/under_construction_page.dart';
 import 'package:flutter/material.dart';
 
+import 'core/custom_theme.dart';
+
 class MyResumeApp extends StatelessWidget {
   const MyResumeApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Material App',
-      home: UnderConstructionPage(),
+      theme: CustomTheme.light,
+      home: const UnderConstructionPage(),
     );
   }
 }
