d788a373e1ae5ccfcf1a6494768fee38a1431741
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-15 17:51:51 +0100

    :lipstick: update theme

--- lib/presentation/core/custom_theme.dart
+++ lib/presentation/core/custom_theme.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 
 class CustomTheme {
   static const Color primaryText1 = Colors.green;
+  static const Color primaryText2 = Colors.amber;
   static ThemeData get light {
     final defaultDarkTheme = ThemeData.dark();
     return defaultDarkTheme.copyWith(
       textTheme: defaultDarkTheme.textTheme.copyWith(
         headline1: const TextStyle(
-          color: primaryText1,
-          fontSize: 50,
-          fontWeight: FontWeight.bold,
-        ),
+            color: primaryText2,
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Arcade'),
       ),
     );
   }
