89c9682aec8f9b5f347a940d5fbd45ec07efbeec
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-15 14:05:51 +0100

    :lipstick: update layout

--- lib/presentation/under_construction_page.dart
+++ lib/presentation/under_construction_page.dart
@@ -1,3 +1,4 @@
+import 'package:antoniomdm/shared/values/image_path.dart';
 import 'package:flutter/material.dart';
 
 class UnderConstructionPage extends StatelessWidget {
@@ -8,19 +9,56 @@ class UnderConstructionPage extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: FittedBox(
-            child: Column(
-              children: [
-                Text(
-                  'Antonio Manuel Díaz Moreno',
-                  style: Theme.of(context).textTheme.headline1,
-                ),
-                Text(
-                  'Under Construction',
-                  style: Theme.of(context).textTheme.headlineLarge,
-                ),
-              ],
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(ImagePath.bg3),
+              fit: BoxFit.cover,
+              alignment: Alignment.center,
+            ),
+          ),
+          child: Center(
+            child: SizedBox(
+              width: 900,
+              child: Stack(
+                children: [
+                  const Image(
+                    image: AssetImage(ImagePath.arcade),
+                    fit: BoxFit.fitWidth,
+                    alignment: Alignment.center,
+                  ),
+                  Positioned(
+                    top: 230,
+                    left: 195,
+                    width: 500,
+                    height: 380,
+                    child: Transform(
+                      transform: Matrix4.identity()
+                        ..setEntry(3, 2, 0.001)
+                        ..rotateX(-.4),
+                      alignment: FractionalOffset.center,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Antonio Manuel Díaz Moreno',
+                              style: Theme.of(context).textTheme.headline1,
+                            ),
+                            Text(
+                              'Under Construction',
+                              style: Theme.of(context).textTheme.headlineLarge,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
---
+++ lib/shared/values/image_path.dart
@@ -0,0 +1,7 @@
+class ImagePath {
+  static const String imageDir = 'assets/img';
+  static const String bg1 = '$imageDir/bg_01.jpg';
+  static const String bg2 = '$imageDir/bg_02.jpg';
+  static const String bg3 = '$imageDir/bg_03.png';
+  static const String arcade = '$imageDir/arcade.png';
+}
