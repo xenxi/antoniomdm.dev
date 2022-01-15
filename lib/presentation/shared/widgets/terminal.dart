6e442f34e9a70e0b326b5a6a4f3906f7d3ff8b6d
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-15 20:51:07 +0100

    :dizzy: add animation

--- lib/presentation/shared/widgets/terminal.dart
+++ lib/presentation/shared/widgets/terminal.dart
@@ -1,44 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class Terminal extends StatelessWidget {
+class Terminal extends HookWidget {
   const Terminal({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final background = TweenSequence<Color?>([
+      TweenSequenceItem(
+        weight: 1.0,
+        tween: ColorTween(
+          begin: Colors.green.withOpacity(.5),
+          end: Colors.transparent,
+        ),
+      ),
+    ]);
+    final controller =
+        useAnimationController(duration: const Duration(milliseconds: 1500))
+          ..repeat();
     return Expanded(
       child: Stack(
         children: [
-          Container(
-            color: Colors.green.withOpacity(.5),
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 50),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  'Antonio Manuel Díaz Moreno',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headline1,
+          AnimatedBuilder(
+            builder: (BuildContext context, Widget? child) {
+              return Container(
+                color: background
+                    .evaluate(AlwaysStoppedAnimation(controller.value)),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 50),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Antonio Manuel Díaz Moreno',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.headline1,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Developer | .NET | TypeScript | Dart | Angular | Flutter 🚀',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                    const Spacer(),
+                    Text(
+                      'Under Construction',
+                      style: Theme.of(context).textTheme.headline2,
+                    ),
+                    const Spacer(),
+                  ],
                 ),
-                const SizedBox(height: 10),
-                Text(
-                  'Developer | .NET | TypeScript | Dart | Angular | Flutter 🚀',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.subtitle1,
-                ),
-                const Spacer(),
-                Text(
-                  'Under Construction',
-                  style: Theme.of(context).textTheme.headline2,
-                ),
-                const Spacer(),
-              ],
-            ),
+              );
+            },
+            animation: controller,
           ),
         ],
       ),
     );
+    // }
+
+    // Animatable<Color> background = TweenSequence<Color>([
+    //   TweenSequenceItem(
+    //     weight: 1.0,
+    //     tween: ColorTween(
+    //       begin: Colors.blueAccent,
+    //       end: Colors.greenAccent,
+    //     ),
+    //   ),
+    //   TweenSequenceItem(
+    //     weight: 1.0,
+    //     tween: ColorTween(
+    //       begin: Colors.greenAccent,
+    //       end: Colors.pinkAccent,
+    //     ),
+    //   ),
+    //   TweenSequenceItem(
+    //     weight: 1.0,
+    //     tween: ColorTween(
+    //       begin: Colors.pinkAccent,
+    //       end: Colors.orangeAccent,
+    //     ),
+    //   ),
+    //   TweenSequenceItem(
+    //     weight: 1.0,
+    //     tween: ColorTween(
+    //       begin: Colors.orangeAccent,
+    //       end: Colors.blueAccent,
+    //     ),
+    //   ),
+    // ]);
   }
 }
