1b4ca033c54bc8646298b3038ba9eefe5c0da6e5
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-15 14:08:41 +0100

    :recycle: refact extract class

--- lib/presentation/under_construction_page.dart
+++ lib/presentation/under_construction_page.dart
@@ -1,6 +1,8 @@
 import 'package:antoniomdm/shared/values/image_path.dart';
 import 'package:flutter/material.dart';
 
+import 'ardade.dart';
+
 class UnderConstructionPage extends StatelessWidget {
   const UnderConstructionPage({Key? key}) : super(key: key);
 
@@ -17,49 +19,8 @@ class UnderConstructionPage extends StatelessWidget {
               alignment: Alignment.center,
             ),
           ),
-          child: Center(
-            child: SizedBox(
-              width: 900,
-              child: Stack(
-                children: [
-                  const Image(
-                    image: AssetImage(ImagePath.arcade),
-                    fit: BoxFit.fitWidth,
-                    alignment: Alignment.center,
-                  ),
-                  Positioned(
-                    top: 230,
-                    left: 195,
-                    width: 500,
-                    height: 380,
-                    child: Transform(
-                      transform: Matrix4.identity()
-                        ..setEntry(3, 2, 0.001)
-                        ..rotateX(-.4),
-                      alignment: FractionalOffset.center,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 8, vertical: 20),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Antonio Manuel Díaz Moreno',
-                              style: Theme.of(context).textTheme.headline1,
-                            ),
-                            Text(
-                              'Under Construction',
-                              style: Theme.of(context).textTheme.headlineLarge,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
+          child: const Center(
+            child: Arcade(),
           ),
         ),
       ),
