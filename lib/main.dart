beb0f030d01f4056c67275659e0cb012ba601276
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-19 09:58:01 +0100

    :triangular_flag_on_post: add router navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:antoniomdm/presentation/my_resume_app.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyResumeApp());
+void main() => runApp(MyResumeApp());
--- test/my_resume_app_test.dart
+++ test/my_resume_app_test.dart
@@ -4,11 +4,11 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('my resume should', () {
     testWidgets('show name', (WidgetTester tester) async {
-      await tester.pumpWidget(const MyResumeApp());
+      await tester.pumpWidget(MyResumeApp());
       expect(find.text('Antonio Manuel Díaz Moreno'), findsOneWidget);
     });
     testWidgets('show under construction', (WidgetTester tester) async {
-      await tester.pumpWidget(const MyResumeApp());
+      await tester.pumpWidget(MyResumeApp());
       expect(find.text('Under Construction'), findsOneWidget);
     });
   });
