83d465780891ea27a6b95ff8e06e91547a6a5cf7
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-19 18:08:33 +0100

    :lipstick: update view

--- lib/presentation/views/under_construction_view.dart
+++ lib/presentation/views/under_construction_view.dart
@@ -12,33 +12,36 @@ class UnderConstructionView extends HookWidget {
     final nameIsVisible = useState(false);
     return Stack(
       children: [
-        Column(
-          children: [
-            const SizedBox(height: 20),
-            AnimatedTextKit(
-              isRepeatingAnimation: false,
-              onFinished: () => nameIsVisible.value = true,
-              animatedTexts: [
-                TypewriterAnimatedText('Antonio Manuel Díaz Moreno',
-                    speed: const Duration(milliseconds: 100),
-                    textStyle: Theme.of(context).textTheme.headline1,
-                    textAlign: TextAlign.center),
-              ],
-            ),
-            const SizedBox(height: 10),
-            if (nameIsVisible.value)
+        Align(
+          alignment: Alignment.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
               AnimatedTextKit(
                 isRepeatingAnimation: false,
+                onFinished: () => nameIsVisible.value = true,
                 animatedTexts: [
-                  TypewriterAnimatedText(
-                    'Developer | .NET | TypeScript | Dart | Angular | Flutter 🚀',
-                    textStyle: Theme.of(context).textTheme.subtitle1,
-                    textAlign: TextAlign.center,
-                    speed: const Duration(milliseconds: 100),
-                  ),
+                  TypewriterAnimatedText('Antonio Manuel Díaz Moreno',
+                      speed: const Duration(milliseconds: 100),
+                      textStyle: Theme.of(context).textTheme.headline1,
+                      textAlign: TextAlign.center),
                 ],
               ),
-          ],
+              const SizedBox(height: 10),
+              if (nameIsVisible.value)
+                AnimatedTextKit(
+                  isRepeatingAnimation: false,
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      'Developer | .NET | TypeScript | Dart | Angular | Flutter 🚀',
+                      textStyle: Theme.of(context).textTheme.subtitle1,
+                      textAlign: TextAlign.center,
+                      speed: const Duration(milliseconds: 100),
+                    ),
+                  ],
+                ),
+            ],
+          ),
         ),
         Align(
           alignment: Alignment.bottomCenter,
