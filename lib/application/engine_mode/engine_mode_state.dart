4f4d507ae5a08464cb37a823a2352a2cbcdf0b5a
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-20 11:56:51 +0100

    :white_check_mark: green test

--- lib/application/engine_mode/engine_mode_state.dart
+++ lib/application/engine_mode/engine_mode_state.dart
@@ -1,6 +1,9 @@
 part of 'engine_mode_bloc.dart';
 
 @immutable
-abstract class EngineModeState {}
+abstract class EngineModeState extends Equatable {}
 
-class EngineModeWindows extends EngineModeState {}
+class EngineModeWindows extends EngineModeState {
+  @override
+  List<Object?> get props => [];
+}
