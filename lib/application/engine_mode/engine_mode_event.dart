cf6bf415b2b32bedc7875478c219e3552f5447dc
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-20 12:10:49 +0100

    :white_check_mark: green test: switch to arcade mode

--- lib/application/engine_mode/engine_mode_event.dart
+++ lib/application/engine_mode/engine_mode_event.dart
@@ -1,4 +1,11 @@
 part of 'engine_mode_bloc.dart';
 
 @immutable
-abstract class EngineModeEvent {}
+abstract class EngineModeEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class ArcadeEngineModeSelected extends EngineModeEvent {}
+
+class WindowsEngineModeSelected extends EngineModeEvent {}
