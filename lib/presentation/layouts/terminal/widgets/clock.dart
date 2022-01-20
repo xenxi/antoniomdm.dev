9161ffc975fe6ed78a89240d195e1fc4501cd661
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-20 20:27:21 +0100

    :lipstick: update clock

--- lib/presentation/layouts/terminal/widgets/clock.dart
+++ lib/presentation/layouts/terminal/widgets/clock.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:intl/intl.dart';
 
-class Clock extends StatelessWidget {
-  static final DateFormat timeFormat = DateFormat('kk:mm');
+class Clock extends HookWidget {
+  static final DateFormat timeFormat = DateFormat('kk:mm:ss');
   static final DateFormat dateFormat = DateFormat('dd/MM/yyyy');
   const Clock({
     Key? key,
@@ -10,14 +11,21 @@ class Clock extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final currentDateTime = DateTime.now();
+    final controller = useAnimationController(
+      duration: const Duration(seconds: 1),
+    )..repeat();
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(timeFormat.format(currentDateTime)),
-        Text(dateFormat.format(currentDateTime)),
-      ],
-    );
+    return AnimatedBuilder(
+        animation: controller,
+        builder: (context, _) {
+          final currentDateTime = DateTime.now();
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(timeFormat.format(currentDateTime)),
+              Text(dateFormat.format(currentDateTime)),
+            ],
+          );
+        });
   }
 }
