0296ffa2b38b59e3cb5617cbbcef0ed956473711
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-20 08:19:45 +0100

    :lipstick: update background

--- lib/shared/values/image_path.dart
+++ lib/shared/values/image_path.dart
@@ -5,6 +5,8 @@ class ImagePath {
   static const String bg3 = '$imageDir/bg_03.png';
   static const String bg4 = '$imageDir/bg_04.jpg';
   static const String bg5 = '$imageDir/bg_05.jpg';
+  static const String bg6 = '$imageDir/bg_06.jpg';
+  static const String bg7 = '$imageDir/bg_07.jpg';
   static const String animatedBg1 = '$imageDir/animate_bg_01.gif';
   static const String animatedBg2 = '$imageDir/animate_bg_02.gif';
   static const String arcade = '$imageDir/arcade.png';
