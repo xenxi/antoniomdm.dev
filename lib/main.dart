81a137ae884cdec2c2e5821a113f8c73b06f02c0
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-20 16:54:15 +0100

    :memo: update metas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:antoniomdm/presentation/my_resume_app.dart';
+import 'package:amdiaz/presentation/my_resume_app.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyResumeApp());
--- lib/presentation/views/under_construction_view.dart
+++ lib/presentation/views/under_construction_view.dart
@@ -1,5 +1,5 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:antoniomdm/shared/values/location.dart';
+import 'package:amdiaz/shared/values/location.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
