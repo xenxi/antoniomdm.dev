9012b87c1b06ea982881669b961ae4e426a66653
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-20 19:59:40 +0100

    :lipstick: update clock

--- lib/presentation/layouts/terminal/widgets/clock.dart
+++ lib/presentation/layouts/terminal/widgets/clock.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Clock extends StatelessWidget {
+  static final DateFormat timeFormat = DateFormat('kk:mm');
+  static final DateFormat dateFormat = DateFormat('dd/MM/yyyy');
   const Clock({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final currentDateTime = DateTime.now();
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: const [
-        Text('8:00'),
-        Text('20/01/2022'),
+      children: [
+        Text(timeFormat.format(currentDateTime)),
+        Text(dateFormat.format(currentDateTime)),
       ],
     );
   }
