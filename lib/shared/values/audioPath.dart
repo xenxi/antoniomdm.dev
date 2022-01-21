480ea63809217a0db11a4d1422bc6f810db98a6a
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-21 07:42:07 +0100

    :recycle: refact introduce constant

---
+++ lib/shared/values/audioPath.dart
@@ -0,0 +1,4 @@
+class AudioPath {
+  static const String _root = 'assets/img';
+  static const String arcadeMusic1 = '$_root/arcade_01.mp3';
+}
--- lib/shared/values/image_path.dart
+++ lib/shared/values/image_path.dart
@@ -1,13 +1,13 @@
 class ImagePath {
-  static const String imageDir = 'assets/img';
-  static const String bg1 = '$imageDir/bg_01.jpg';
-  static const String bg2 = '$imageDir/bg_02.jpg';
-  static const String bg3 = '$imageDir/bg_03.png';
-  static const String bg4 = '$imageDir/bg_04.jpg';
-  static const String bg5 = '$imageDir/bg_05.jpg';
-  static const String bg6 = '$imageDir/bg_06.jpg';
-  static const String bg7 = '$imageDir/bg_07.jpg';
-  static const String animatedBg1 = '$imageDir/animate_bg_01.gif';
-  static const String animatedBg2 = '$imageDir/animate_bg_02.gif';
-  static const String arcade = '$imageDir/arcade.png';
+  static const String _root = 'assets/img';
+  static const String bg1 = '$_root/bg_01.jpg';
+  static const String bg2 = '$_root/bg_02.jpg';
+  static const String bg3 = '$_root/bg_03.png';
+  static const String bg4 = '$_root/bg_04.jpg';
+  static const String bg5 = '$_root/bg_05.jpg';
+  static const String bg6 = '$_root/bg_06.jpg';
+  static const String bg7 = '$_root/bg_07.jpg';
+  static const String animatedBg1 = '$_root/animate_bg_01.gif';
+  static const String animatedBg2 = '$_root/animate_bg_02.gif';
+  static const String arcade = '$_root/arcade.png';
 }
