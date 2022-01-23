c1a1d2cf3b41367ecb37e4c18823d82018477baa
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-23 09:07:16 +0100

    :recycle: refact

--- lib/presentation/layouts/arcade/widgets/ambient_music.dart
+++ lib/presentation/layouts/arcade/widgets/ambient_music.dart
@@ -1,5 +1,5 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 import '../../../../shared/values/audioPath.dart';
 
 class AmbientMusic extends StatefulWidget {
@@ -10,23 +10,24 @@ class AmbientMusic extends StatefulWidget {
 }
 
 class _AmbientMusicState extends State<AmbientMusic> {
-  static AudioCache cache = AudioCache();
-  AudioPlayer soundManager = AudioPlayer();
+  final player = AudioPlayer();
   late bool _isPlaying;
+  Duration? _duration;
   @override
   void initState() {
     super.initState();
     _isPlaying = false;
-    // _play();
-    // soundManager.onPlayerStateChanged.listen((state) {
-    //   if (state == PlayerState.COMPLETED) {
-    //     _stop();
-    //   }
-    // });
+    player.setAsset(AudioPath.arcadeMusic1).then((value) {
+      setState(() {
+        _play();
+        _duration = value;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    if (_duration == null) return Container();
     return IconButton(
         onPressed: () async => _isPlaying ? await _stop() : await _play(),
         icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow));
@@ -35,23 +36,21 @@ class _AmbientMusicState extends State<AmbientMusic> {
   @override
   void dispose() {
     super.dispose();
-    soundManager.dispose();
+    player.dispose();
   }
 
   Future<void> _play() async {
-    soundManager = await cache.play(AudioPath.arcadeMusic1);
-    // await soundManager.play(AudioPath.arcadeMusic1,
-    //     isLocal: true, position: const Duration(seconds: 30));
-
     setState(() {
       _isPlaying = true;
     });
+    return player.play();
   }
 
   Future<void> _stop() async {
-    await soundManager.pause();
     setState(() {
       _isPlaying = false;
     });
+
+    return player.pause();
   }
 }
