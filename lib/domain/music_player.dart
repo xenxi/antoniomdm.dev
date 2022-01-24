5ccb1b201179e6c1ff867e9bb8d2dad118b2f4a0
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-24 07:12:22 +0100

    :fire: remove unused code

--- lib/domain/music_player.dart
+++ lib/domain/music_player.dart
@@ -1,12 +1,9 @@
 import 'package:dartz/dartz.dart';
 
+import 'failures.dart';
+
 abstract class MusicPlayer {
   Future<Either<Failure, Unit>> play(String filePath);
   Future<Either<Failure, Unit>> resume();
   Future<Either<Failure, Unit>> pause();
-}
-
-class Failure {
-  final String message;
-  Failure({this.message = 'Something went wrong'});
 }
