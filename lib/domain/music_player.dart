946d7d723f708b0e010a3efd3c369a53920a8cf8
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-24 07:09:15 +0100

    :fire: remove unused code

--- lib/domain/music_player.dart
+++ lib/domain/music_player.dart
@@ -4,7 +4,6 @@ abstract class MusicPlayer {
   Future<Either<Failure, Unit>> play(String filePath);
   Future<Either<Failure, Unit>> resume();
   Future<Either<Failure, Unit>> pause();
-  Future<Either<Failure, Unit>> stop();
 }
 
 class Failure {
