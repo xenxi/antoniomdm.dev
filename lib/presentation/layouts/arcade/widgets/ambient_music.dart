631f70fa538148421a8103277cd14f680a3ddbdd
xenxi <antoniom.diaz.moreno@gmail.com>
2022-01-24 08:56:03 +0100

    :dizzy: update ambient

--- lib/presentation/layouts/arcade/widgets/ambient_music.dart
+++ lib/presentation/layouts/arcade/widgets/ambient_music.dart
@@ -1,26 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import '../../../../application/engine_mode/engine_mode_bloc.dart';
 
-class AmbientMusic extends StatelessWidget {
+class AmbientMusic extends HookWidget {
   const AmbientMusic({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final controller =
+        useAnimationController(duration: const Duration(milliseconds: 500));
     return BlocConsumer<EngineModeBloc, EngineModeState>(
-      listener: (context, state) {},
+      listener: (context, state) {
+        if (state is EngineModeArcade) {
+          state.playingBackgroundMusicOption.fold(() => {}, (isPlaying) {
+            if (isPlaying) {
+              controller.forward();
+            } else {
+              controller.reverse();
+            }
+          });
+        }
+      },
       builder: (context, state) {
-        if (state is EngineModeArcade &&
-            state.playingBackgroundMusicOption.isSome()) {
-          final isPlaying =
-              state.playingBackgroundMusicOption.getOrElse(() => false);
+        if (state is EngineModeArcade) {
           return IconButton(
-              onPressed: () => isPlaying
-                  ? BlocProvider.of<EngineModeBloc>(context)
-                      .add(PauseBackgroundMusicSelected())
-                  : BlocProvider.of<EngineModeBloc>(context)
-                      .add(ResumeBackgroundMusicSelected()),
-              icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow));
+              onPressed: state.playingBackgroundMusicOption.fold(
+                  () => null,
+                  (isPlaying) => () => isPlaying
+                      ? BlocProvider.of<EngineModeBloc>(context)
+                          .add(PauseBackgroundMusicSelected())
+                      : BlocProvider.of<EngineModeBloc>(context)
+                          .add(ResumeBackgroundMusicSelected())),
+              icon: AnimatedIcon(
+                  icon: AnimatedIcons.play_pause, progress: controller));
         }
         return Container();
       },
