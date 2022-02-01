5221f2700f676932b716dbf7b865014ad39b44d9
xenxi <antoniom.diaz.moreno@gmail.com>
2022-02-01 07:08:04 +0100

    :recycle: refact extract method

--- lib/presentation/layouts/windows/windows_layout.dart
+++ lib/presentation/layouts/windows/windows_layout.dart
@@ -25,26 +25,7 @@ class WindowsLayout extends StatelessWidget {
           Positioned(top: 20, left: 20, child: _buildDesktopIcon(context)),
           Align(
             alignment: Alignment.center,
-            child: FittedBox(
-              child: SizedBox(
-                height: 500,
-                width: 600,
-                child: Card(
-                  clipBehavior: Clip.antiAlias,
-                  margin: const EdgeInsets.all(20),
-                  elevation: 8,
-                  child: Column(
-                    children: [
-                      const PopupOptionsBar(),
-                      Expanded(child: child),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            child: _buildModalWindows(),
           ),
           const Align(
             alignment: Alignment.bottomCenter,
@@ -55,21 +36,40 @@ class WindowsLayout extends StatelessWidget {
     );
   }
 
-  Widget _buildDesktopIcon(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        IconButton(
-            onPressed: () {
-              final bloc = BlocProvider.of<EngineModeBloc>(context)
-                ..add(ArcadeEngineModeSelected());
-              Future.delayed(const Duration(milliseconds: 500),
-                  () => bloc.add(PlayBackgroundMusicSelected()));
-            },
-            icon: const FaIcon(FontAwesomeIcons.gamepad)),
-        const Text('Arcade Mode')
-      ],
-    );
-  }
+  Widget _buildModalWindows() => FittedBox(
+        child: SizedBox(
+          height: 500,
+          width: 600,
+          child: Card(
+            clipBehavior: Clip.antiAlias,
+            margin: const EdgeInsets.all(20),
+            elevation: 8,
+            child: Column(
+              children: [
+                const PopupOptionsBar(),
+                Expanded(child: child),
+                const SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+
+  Widget _buildDesktopIcon(BuildContext context) => Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IconButton(
+              onPressed: () {
+                final bloc = BlocProvider.of<EngineModeBloc>(context)
+                  ..add(ArcadeEngineModeSelected());
+                Future.delayed(const Duration(milliseconds: 500),
+                    () => bloc.add(PlayBackgroundMusicSelected()));
+              },
+              icon: const FaIcon(FontAwesomeIcons.gamepad)),
+          const Text('Arcade Mode')
+        ],
+      );
 }
