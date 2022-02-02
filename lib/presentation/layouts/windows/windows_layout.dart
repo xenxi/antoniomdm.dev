628b811cdabb0df176b78bab22a9c99224676276
xenxi <antoniom.diaz.moreno@gmail.com>
2022-02-02 07:52:07 +0100

    :recycle: refact inline method

--- lib/presentation/layouts/windows/windows_layout.dart
+++ lib/presentation/layouts/windows/windows_layout.dart
@@ -38,7 +38,14 @@ class WindowsLayout extends HookWidget {
                   child: Image(
                       image: AssetImage(ImagePath.bg7), fit: BoxFit.cover),
                 ),
-                _buildDraggableDesktopIcon(context, offset: iconOffset),
+                Positioned(
+                  top: iconOffset.value.dy,
+                  left: iconOffset.value.dx,
+                  child: DraggableContainer(
+                    onDragEnd: (details) => iconOffset.value = details.offset,
+                    child: _buildDesktopIcon(context),
+                  ),
+                ),
                 _buildDraggableModalWindows(context,
                     offset: modalOffset,
                     modalExpanded: modalExpanded,
@@ -82,7 +89,7 @@ class WindowsLayout extends HookWidget {
           required ValueNotifier<bool> modalMinimized}) =>
       FittedBox(
         child: AnimatedContainer(
-          duration: const Duration(milliseconds: 800),
+          duration: const Duration(milliseconds: 300),
           curve: Curves.elasticInOut,
           height: size.height,
           width: size.width,
@@ -107,16 +114,6 @@ class WindowsLayout extends HookWidget {
         500 > currentSize.height - 50 ? currentSize.height - 50 : 500);
   }
 
-  Widget _buildDraggableDesktopIcon(BuildContext context,
-          {required ValueNotifier<Offset> offset}) =>
-      Positioned(
-        top: offset.value.dy,
-        left: offset.value.dx,
-        child: DraggableContainer(
-          onDragEnd: (details) => offset.value = details.offset,
-          child: _buildDesktopIcon(context),
-        ),
-      );
   Widget _buildDesktopIcon(BuildContext context) => InkWell(
         onTap: () {
           final bloc = BlocProvider.of<EngineModeBloc>(context)
