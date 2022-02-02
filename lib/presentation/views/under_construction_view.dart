c222f8148ff576f5f4ffa321bc2254f3a35f81db
xenxi <antoniom.diaz.moreno@gmail.com>
2022-02-02 07:25:47 +0100

    :recycle: refact

--- lib/presentation/views/under_construction_view.dart
+++ lib/presentation/views/under_construction_view.dart
@@ -11,49 +11,51 @@ class UnderConstructionView extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final nameIsVisible = useState(false);
+    final subtitleIsVisible = useState(false);
     return Stack(
       children: [
-        Align(
-          alignment: Alignment.center,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            AnimatedTextKit(
+              isRepeatingAnimation: false,
+              onFinished: () => nameIsVisible.value = true,
+              animatedTexts: [
+                TypewriterAnimatedText(Location.fullName,
+                    speed: const Duration(milliseconds: 100),
+                    textStyle: Theme.of(context).textTheme.headline1,
+                    textAlign: TextAlign.center),
+              ],
+            ),
+            const SizedBox(height: 10),
+            if (nameIsVisible.value)
               AnimatedTextKit(
                 isRepeatingAnimation: false,
-                onFinished: () => nameIsVisible.value = true,
+                onFinished: () => subtitleIsVisible.value = true,
                 animatedTexts: [
-                  TypewriterAnimatedText(Location.fullName,
-                      speed: const Duration(milliseconds: 100),
-                      textStyle: Theme.of(context).textTheme.headline1,
-                      textAlign: TextAlign.center),
+                  TypewriterAnimatedText(
+                    'Developer | .NET | TypeScript | Dart | Angular | Flutter 🚀',
+                    textStyle: Theme.of(context).textTheme.subtitle1,
+                    textAlign: TextAlign.center,
+                    speed: const Duration(milliseconds: 100),
+                  ),
                 ],
               ),
-              const SizedBox(height: 10),
-              if (nameIsVisible.value)
-                AnimatedTextKit(
-                  isRepeatingAnimation: false,
-                  animatedTexts: [
-                    TypewriterAnimatedText(
-                      'Developer | .NET | TypeScript | Dart | Angular | Flutter 🚀',
-                      textStyle: Theme.of(context).textTheme.subtitle1,
-                      textAlign: TextAlign.center,
-                      speed: const Duration(milliseconds: 100),
-                    ),
-                  ],
-                ),
-            ],
-          ),
+            const Spacer(
+              flex: 3,
+            ),
+            if (subtitleIsVisible.value)
+              AnimatedTextKit(
+                repeatForever: true,
+                animatedTexts: [
+                  FlickerAnimatedText('Under Construction',
+                      textStyle: Theme.of(context).textTheme.headline2),
+                ],
+              ),
+            const Spacer(),
+          ],
         ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: AnimatedTextKit(
-            repeatForever: true,
-            animatedTexts: [
-              FlickerAnimatedText('Under Construction',
-                  textStyle: Theme.of(context).textTheme.headline2),
-            ],
-          ),
-        ),
+
         // const Spacer(),
       ],
     );
