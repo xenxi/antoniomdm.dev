482a00a75d423339497de526419b5f2ca7f48621
xenxi <antoniom.diaz.moreno@gmail.com>
2022-02-09 06:38:25 +0100

    :recycle: refact extract class

---
+++ lib/presentation/layouts/windows/widgets/windows_navigation_bar/sub_menu_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SubMenuButton extends StatelessWidget {
+  final void Function()? onTap;
+  final void Function(bool val)? onHover;
+  final bool showText;
+  final double width;
+  final String text;
+  final IconData icon;
+
+  const SubMenuButton({
+    Key? key,
+    this.onTap,
+    this.onHover,
+    required this.showText,
+    required this.width,
+    required this.text,
+    required this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8),
+      child: InkWell(
+        onHover: onHover,
+        onTap: onTap,
+        child: Row(
+          children: [
+            SizedBox(
+                width: width, child: Center(child: FaIcon(icon, size: 20))),
+            if (showText) Text(text)
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/layouts/windows/widgets/windows_navigation_bar/vertical_sub_menu.dart
+++ lib/presentation/layouts/windows/widgets/windows_navigation_bar/vertical_sub_menu.dart
@@ -1,3 +1,4 @@
+import 'package:amdiaz/presentation/layouts/windows/widgets/windows_navigation_bar/sub_menu_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -24,37 +25,36 @@ class VerticalSubMenu extends HookWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 8),
-              child: InkWell(
-                // onHover: (val) => isExpanded.value = val,
-                onTap: () => isExpanded.value = !isExpanded.value,
-                child: Row(
-                  children: [
-                    SizedBox(
-                        width: width,
-                        child: const Center(child: Icon(Icons.menu, size: 20))),
-                    const Text('INICIO')
-                  ],
-                ),
-              ),
-            ),
+            SubMenuButton(
+                showText: isExpanded.value,
+                width: width,
+                icon: Icons.menu,
+                text: 'INICIO',
+                onTap: () => isExpanded.value = !isExpanded.value),
             const Spacer(),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.settings),
+            SubMenuButton(
+              showText: isExpanded.value,
+              width: width,
+              icon: Icons.settings,
+              text: 'Configuración',
             ),
-            IconButton(
-              onPressed: () {},
-              icon: FaIcon(FontAwesomeIcons.images),
+            SubMenuButton(
+              showText: isExpanded.value,
+              width: width,
+              icon: FontAwesomeIcons.images,
+              text: 'Imágenes',
             ),
-            IconButton(
-              onPressed: () {},
-              icon: FaIcon(FontAwesomeIcons.xbox),
+            SubMenuButton(
+              showText: isExpanded.value,
+              width: width,
+              icon: FontAwesomeIcons.xbox,
+              text: 'Juegos',
             ),
-            IconButton(
-              onPressed: () {},
-              icon: FaIcon(FontAwesomeIcons.powerOff),
+            SubMenuButton(
+              showText: isExpanded.value,
+              width: width,
+              icon: FontAwesomeIcons.powerOff,
+              text: 'Apagar',
             ),
           ]),
     );
