ba2d438d4ed98cf0ae01c97d93d6f2d56fa436c2
xenxi <antoniom.diaz.moreno@gmail.com>
2022-02-09 06:30:57 +0100

    :lipstick: update submenu

--- lib/presentation/layouts/windows/widgets/windows_navigation_bar/vertical_sub_menu.dart
+++ lib/presentation/layouts/windows/widgets/windows_navigation_bar/vertical_sub_menu.dart
@@ -1,38 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class VerticalSubMenu extends StatelessWidget {
+class VerticalSubMenu extends HookWidget {
+  final double width;
   const VerticalSubMenu({
     Key? key,
+    required this.width,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 49.5,
-      child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-        IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.menu),
-        ),
-        const Spacer(),
-        IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.settings),
-        ),
-        IconButton(
-          onPressed: () {},
-          icon: FaIcon(FontAwesomeIcons.images),
-        ),
-        IconButton(
-          onPressed: () {},
-          icon: FaIcon(FontAwesomeIcons.xbox),
-        ),
-        IconButton(
-          onPressed: () {},
-          icon: FaIcon(FontAwesomeIcons.powerOff),
-        ),
-      ]),
+    final isExpanded = useState(false);
+    return AnimatedContainer(
+      decoration:
+          BoxDecoration(color: Theme.of(context).cardColor.withOpacity(.3)),
+      duration: const Duration(milliseconds: 600),
+      curve: Curves.slowMiddle,
+      clipBehavior: Clip.hardEdge,
+      width: isExpanded.value ? width * 3 : width,
+      child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 8),
+              child: InkWell(
+                // onHover: (val) => isExpanded.value = val,
+                onTap: () => isExpanded.value = !isExpanded.value,
+                child: Row(
+                  children: [
+                    SizedBox(
+                        width: width,
+                        child: const Center(child: Icon(Icons.menu, size: 20))),
+                    const Text('INICIO')
+                  ],
+                ),
+              ),
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.settings),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: FaIcon(FontAwesomeIcons.images),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: FaIcon(FontAwesomeIcons.xbox),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: FaIcon(FontAwesomeIcons.powerOff),
+            ),
+          ]),
     );
   }
 }
