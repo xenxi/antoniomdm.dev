1c9f5281c5866dd5a1bffe7ee436670ff008531d
xenxi <antoniom.diaz.moreno@gmail.com>
2022-02-09 05:41:17 +0100

    :recycle: refact extract class

--- lib/presentation/layouts/windows/widgets/windows_navigation_bar/windows_navigation_bar.dart
+++ lib/presentation/layouts/windows/widgets/windows_navigation_bar/windows_navigation_bar.dart
@@ -1,11 +1,11 @@
-import 'package:amdiaz/presentation/core/custom_theme.dart';
+import 'package:amdiaz/presentation/layouts/windows/widgets/windows_navigation_bar/vertica_main_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import '../../../../application/engine_mode/engine_mode_bloc.dart';
-import 'clock.dart';
+import '../../../../../application/engine_mode/engine_mode_bloc.dart';
+import '../clock.dart';
 
 class WindowsNavigationBar extends HookWidget {
   const WindowsNavigationBar({
@@ -19,7 +19,7 @@ class WindowsNavigationBar extends HookWidget {
       mainAxisAlignment: MainAxisAlignment.end,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        _WindowsNavigationBarMenu(isOpen: isOpen.value),
+        VerticalMainMenu(isOpen: isOpen.value),
         _buildMenuBar(context, isOpen: isOpen),
       ],
     );
@@ -78,71 +78,6 @@ class _WindowsButton extends StatelessWidget {
       width: 50,
       child: IconButton(
           onPressed: onPressed, icon: const FaIcon(FontAwesomeIcons.windows)),
-    );
-  }
-}
-
-class _WindowsNavigationBarMenu extends StatelessWidget {
-  final bool isOpen;
-  const _WindowsNavigationBarMenu({
-    Key? key,
-    required this.isOpen,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 400),
-      height: isOpen ? 600 : 0,
-      curve: Curves.fastLinearToSlowEaseIn,
-      width: 300,
-      color: Theme.of(context).primaryColor,
-      child: Row(
-        children: [
-          SizedBox(
-            width: 49.5,
-            child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.menu),
-              ),
-              const Spacer(),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.settings),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: FaIcon(FontAwesomeIcons.images),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: FaIcon(FontAwesomeIcons.xbox),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: FaIcon(FontAwesomeIcons.powerOff),
-              ),
-            ]),
-          ),
-          Expanded(
-            child: ListView(
-              padding: const EdgeInsets.only(top: 10),
-              children: [
-                ListTile(
-                  leading: const FaIcon(FontAwesomeIcons.gamepad),
-                  title: Text(
-                    'Arcade Mode',
-                    style: Theme.of(context).textTheme.button,
-                  ),
-                  onTap: () => BlocProvider.of<EngineModeBloc>(context)
-                      .add(ArcadeEngineModeSelected()),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
