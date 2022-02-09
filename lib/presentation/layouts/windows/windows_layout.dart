85511bb0eb954955c4df8ffdb22b1bf8364e5f1f
xenxi <antoniom.diaz.moreno@gmail.com>
2022-02-09 09:09:08 +0100

    :dizzy: add animation

--- lib/presentation/layouts/windows/windows_layout.dart
+++ lib/presentation/layouts/windows/windows_layout.dart
@@ -1,6 +1,7 @@
 import 'package:amdiaz/presentation/layouts/windows/widgets/windows_navigation_bar/windows_navigation_bar.dart';
 import 'package:amdiaz/presentation/shared/widgets/draggable_container.dart';
 import 'package:amdiaz/shared/values/image_path.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -58,10 +59,12 @@ class WindowsLayout extends HookWidget {
                 child: WindowsNavigationBar(),
               ),
               if (!state.isOn)
-                Container(
-                  height: double.infinity,
-                  width: double.infinity,
-                  color: Colors.black,
+                FlipInX(
+                  child: Container(
+                    height: double.infinity,
+                    width: double.infinity,
+                    color: Colors.black,
+                  ),
                 )
             ],
           );
