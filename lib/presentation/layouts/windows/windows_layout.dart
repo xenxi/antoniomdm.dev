6ffb6033a17dc6c2b3e029231428d24953b5557c
xenxi <antoniom.diaz.moreno@gmail.com>
2022-02-09 18:20:51 +0100

    :lipstick: update turnoff

--- lib/presentation/layouts/windows/windows_layout.dart
+++ lib/presentation/layouts/windows/windows_layout.dart
@@ -2,6 +2,7 @@ import 'package:amdiaz/presentation/layouts/windows/widgets/windows_navigation_b
 import 'package:amdiaz/presentation/shared/widgets/draggable_container.dart';
 import 'package:amdiaz/shared/values/image_path.dart';
 import 'package:animate_do/animate_do.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -59,13 +60,31 @@ class WindowsLayout extends HookWidget {
                 child: WindowsNavigationBar(),
               ),
               if (!state.isOn)
-                FlipInX(
+                FadeIn(
                   child: Container(
                     height: double.infinity,
                     width: double.infinity,
                     color: Colors.black,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const CircularProgressIndicator(color: Colors.purple),
+                        const SizedBox(width: 14),
+                        const Text(
+                          'Apagando',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        AnimatedTextKit(
+                          animatedTexts: [
+                            WavyAnimatedText(' ...'),
+                          ],
+                          isRepeatingAnimation: true,
+                        ),
+                      ],
+                    ),
                   ),
-                )
+                ),
             ],
           );
         },
