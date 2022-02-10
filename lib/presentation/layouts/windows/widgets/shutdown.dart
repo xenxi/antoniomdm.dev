f821e03503583bb5d09e01c52beff25948cd21d2
xenxi <antoniom.diaz.moreno@gmail.com>
2022-02-10 07:58:33 +0100

    :dizzy: update

--- lib/presentation/layouts/windows/widgets/shutdown.dart
+++ lib/presentation/layouts/windows/widgets/shutdown.dart
@@ -9,25 +9,41 @@ class Shutdown extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FadeIn(
-      child: Container(
-        height: double.infinity,
-        width: double.infinity,
-        color: Theme.of(context).cardColor,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            SizedBox(
-                width: 46,
-                height: 46,
-                child: CircularProgressIndicator(color: Colors.white)),
-            SizedBox(height: 14),
-            Text(
-              'Apagando',
-              style: TextStyle(color: Colors.white),
+      child: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: Theme.of(context).cardColor,
+            child: ZoomOut(
+              duration: const Duration(milliseconds: 300),
+              delay: const Duration(milliseconds: 4700),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  SizedBox(
+                      width: 46,
+                      height: 46,
+                      child: CircularProgressIndicator(color: Colors.white)),
+                  SizedBox(height: 14),
+                  Text(
+                    'Apagando',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          FadeIn(
+            delay: const Duration(seconds: 5),
+            child: Container(
+              height: double.infinity,
+              width: double.infinity,
+              color: Colors.black,
+            ),
+          ),
+        ],
       ),
     );
   }
