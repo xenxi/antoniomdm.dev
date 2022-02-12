7b0b194c29c952ec9e14eda0a4959ed01e36deec
xenxi <antoniom.diaz.moreno@gmail.com>
2022-02-12 10:51:53 +0100

    :recycle: refact

--- lib/presentation/layouts/windows/widgets/desktop_icon.dart
+++ lib/presentation/layouts/windows/widgets/desktop_icon.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class DesktopIcon extends StatelessWidget {
+import '../../../shared/widgets/draggable_container.dart';
+
+class DesktopIcon extends HookWidget {
   final void Function()? onTap;
   final IconData icon;
   final String text;
   const DesktopIcon({
+    Key? key,
+    this.onTap,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final iconOffset = useState<Offset>(const Offset(20, 20));
+    return Positioned(
+      top: iconOffset.value.dy,
+      left: iconOffset.value.dx,
+      child: DraggableContainer(
+        feedback: _buildFeedback(context),
+        onDragEnd: (details) => iconOffset.value = details.offset,
+        child: _buildIcon(context),
+      ),
+    );
+  }
+
+  Widget _buildIcon(BuildContext context) => _DesktopIcon(
+        icon: icon,
+        text: text,
+        onTap: onTap,
+      );
+  Widget _buildFeedback(BuildContext context) => _DesktopIcon(
+        icon: icon,
+        text: text,
+      );
+}
+
+class _DesktopIcon extends StatelessWidget {
+  final void Function()? onTap;
+  final IconData icon;
+  final String text;
+  const _DesktopIcon({
     Key? key,
     this.onTap,
     required this.icon,
