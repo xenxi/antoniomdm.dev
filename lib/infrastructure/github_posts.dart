cc0c3b29df9f84bac0a98991c2c1a90ae8564034
xenxi <antoniom.diaz.moreno@gmail.com>
2022-08-06 08:19:43 +0200

    :necktie: add posts

---
+++ lib/infrastructure/github_posts.dart
@@ -0,0 +1,10 @@
+import 'package:amdiaz/domain/posts.dart';
+import 'package:github/github.dart';
+
+class GithubPosts implements Posts {
+  @override
+  Future<Iterable<String>> getAll() {
+    // TODO: implement getAll
+    throw UnimplementedError();
+  }
+}
