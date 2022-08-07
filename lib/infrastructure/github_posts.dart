be6402c4d89a55a38df51d1af1e6105d86e0d39b
xenxi <antoniom.diaz.moreno@gmail.com>
2022-08-07 13:26:23 +0200

    :recycle: introduce field

--- lib/infrastructure/github_posts.dart
+++ lib/infrastructure/github_posts.dart
@@ -4,25 +4,21 @@ import 'package:github/github.dart';
 
 class GithubPosts implements Posts {
   final GitHub github;
-
+  final RepositorySlug slug = RepositorySlug('xenxi', 'blog');
   GithubPosts(this.github);
 
   @override
   Future<Iterable<Post>> getAll() async {
-    var repositorySlug = RepositorySlug('xenxi', 'blog');
+    final posts = await github.repositories.getContents(slug, './Posts/');
 
-    final posts =
-        await github.repositories.getContents(repositorySlug, './Posts/');
-
-    return mapFrom(posts, repositorySlug).toList();
+    return mapFrom(posts).toList();
   }
 
-  Stream<Post> mapFrom(
-      RepositoryContents posts, RepositorySlug repositorySlug) async* {
+  Stream<Post> mapFrom(RepositoryContents posts) async* {
     for (final post in posts.tree ?? <GitHubFile>[]) {
       if (post.type == 'file') {
         final contents =
-            await github.repositories.getContents(repositorySlug, post.path!);
+            await github.repositories.getContents(slug, post.path!);
         final title = post.name!;
         final content = contents.file!.text;
         yield Post(title: title, content: content);
