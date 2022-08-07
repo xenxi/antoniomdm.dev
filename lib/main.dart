ef11b00942e8ee0e6b65f593f34080a9e6af5f27
xenxi <antoniom.diaz.moreno@gmail.com>
2022-08-07 17:30:06 +0200

    :heavy_minus_sign: flutter_dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:amdiaz/presentation/my_resume_app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: ".env");
   runApp(MyResumeApp());
 }
--- test/infrastructure/githup_posts_test.dart
+++ test/infrastructure/githup_posts_test.dart
@@ -1,12 +1,8 @@
-import 'dart:io';
-
 import 'package:amdiaz/infrastructure/github_posts.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:github/github.dart';
 
 void main() {
-  dotenv.testLoad(fileInput: File('assets/.env').readAsStringSync());
   const token = String.fromEnvironment('TOKEN', defaultValue: '');
   final github = GitHub(auth: Authentication.withToken(token));
   group('GithupPosts should', () {
